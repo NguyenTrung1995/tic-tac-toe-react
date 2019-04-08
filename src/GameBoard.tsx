94d0a50494fc8c00856251ff86976c3221aa6398
TrungNQ1 <trung.nguyen@gmail.com>
2019-04-08 10:29:26 +0700

    refactor

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -22,55 +22,45 @@ const ButtonWrapper = styled.button`
     }
 `
 
-class Button extends React.Component<any, any> {
-
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            clicked: false
-        }
-    }
-
-    handleClick() {
-        this.props.togglePlayer();
-        this.setState({clicked: true})
-    }
-
-    render() {
-        return (
-            <ButtonWrapper onClick={() => this.handleClick()}>
-                {this.state.clicked ? this.props.player === false ? 'X' : 'O' : ''}
-            </ButtonWrapper>
-        );
-    }
+const Button = (props: any) => {
+    return(
+        <ButtonWrapper onClick={props.handleClick}>
+            {props.player ? props.player : ''}
+        </ButtonWrapper>
+    );
 }
 
 class GameBoard extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
         this.state = {
-            player: false
+            player: false,
+            listStatusBoard: Array(9).fill({player: '', clicked: false})
         }
-        this.togglePlayer = this.togglePlayer.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    togglePlayer() {
-        console.log('player: ', this.state.player);
-        this.setState({player: !this.state.player})
+    handleClick(index: number) {
+        this.setState({listStatusBoard: this.state.listStatusBoard.map((x: any, i: number) => {
+            if (!x.clicked && i === index) {
+                return {
+                    clicked: true,
+                    player: this.state.player === false ? 'X' : 'O'
+                }
+            }
+            return x;
+        })});
+        this.setState({player: !this.state.player});
     }
 
     render() {
         return (
             <GameBoardWrapper>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
-                <Button togglePlayer={this.togglePlayer} player={this.state.player}/>
+                {this.state.listStatusBoard.map((x: any, index: number) => 
+                    <Button clicked={x.clicked} player={x.player} key={index}
+                        handleClick={() => this.handleClick(index)}
+                    />
+                )}
             </GameBoardWrapper>
         );
     }
